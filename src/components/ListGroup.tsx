7188f14f7134d068488a08d158ae7bee2713016c
Abhi-Jakhar <abhinavjakhar217@gmail.com>
2025-04-16 00:37:36 +0530

    Push on Github

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+}
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setselectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && (
+        <li className="list-group-item">No items found</li>
+      )}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              index === selectedIndex
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setselectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+        ;
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
