74de99496e691cd5999398fdf0828160ba9acc65
Abhi-Jakhar <abhinavjakhar217@gmail.com>
2025-04-18 00:57:01 +0530

    Add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 function App() {
   return (
     <div>
       <Alert>Hello World</Alert>
+      <Button color="success" onClick={() => console.log("Clicked")}>
+        This Button
+      </Button>
     </div>
   );
 }
