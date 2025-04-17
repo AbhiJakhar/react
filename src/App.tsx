d7bfcecd3edeeea6fd152a7128cc878f53259d17
Abhi-Jakhar <abhinavjakhar217@gmail.com>
2025-04-18 01:27:33 +0530

    Add alert open and close functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 function App() {
+  const [alertVisible, setalertVisibility] = useState(false);
   return (
     <div>
-      <Alert>Hello World</Alert>
-      <Button color="success" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setalertVisibility(false)}>Hello World</Alert>
+      )}
+      <Button color="success" onClick={() => setalertVisibility(true)}>
         This Button
       </Button>
     </div>
